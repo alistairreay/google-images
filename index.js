a6457f4d01e73be832ac3a390b152dce1fddf266
Ali Reay <ali@reay.io>
2018-06-14 01:29:34 +0100

    Compiled to ES5 for use with create-react-app

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const qs = require('querystring');
-const got = require('got');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-class Client {
-	constructor(id, apiKey) {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var qs = require('querystring');
+var got = require('got');
+
+var Client = function () {
+	function Client(id, apiKey) {
+		_classCallCheck(this, Client);
+
 		if (!id) {
 			throw new TypeError('Expected a Custom Search Engine ID');
 		}
@@ -18,69 +24,77 @@ class Client {
 		this.id = id;
 	}
 
-	search(query, options) {
-		if (!query) {
-			throw new TypeError('Expected a query');
+	_createClass(Client, [{
+		key: 'search',
+		value: function search(query, options) {
+			if (!query) {
+				throw new TypeError('Expected a query');
+			}
+
+			var url = this.endpoint + '/customsearch/v1?' + this.buildQuery(query, options);
+
+			return got(url, { json: true }).then(function (res) {
+				var items = res.body.items || [];
+
+				return items.map(function (item) {
+					return {
+						type: item.mime,
+						width: item.image.width,
+						height: item.image.height,
+						size: item.image.byteSize,
+						url: item.link,
+						thumbnail: {
+							url: item.image.thumbnailLink,
+							width: item.image.thumbnailWidth,
+							height: item.image.thumbnailHeight
+						},
+						description: item.snippet,
+						parentPage: item.image.contextLink
+					};
+				});
+			});
 		}
+	}, {
+		key: 'buildQuery',
+		value: function buildQuery(query, options) {
+			options = options || {};
 
-		const url = `${this.endpoint}/customsearch/v1?${this.buildQuery(query, options)}`;
+			var result = {
+				q: query.replace(/\s/g, '+'),
+				searchType: 'image',
+				cx: this.id,
+				key: this.apiKey
+			};
 
-		return got(url, {json: true}).then(res => {
-			const items = res.body.items || [];
+			if (options.page) {
+				result.start = options.page;
+			}
 
-			return items.map(item => ({
-				type: item.mime,
-				width: item.image.width,
-				height: item.image.height,
-				size: item.image.byteSize,
-				url: item.link,
-				thumbnail: {
-					url: item.image.thumbnailLink,
-					width: item.image.thumbnailWidth,
-					height: item.image.thumbnailHeight
-				},
-				description: item.snippet,
-				parentPage: item.image.contextLink
-			}));
-		});
-	}
+			if (options.size) {
+				result.imgSize = options.size;
+			}
 
-	buildQuery(query, options) {
-		options = options || {};
+			if (options.type) {
+				result.imgType = options.type;
+			}
 
-		const result = {
-			q: query.replace(/\s/g, '+'),
-			searchType: 'image',
-			cx: this.id,
-			key: this.apiKey
-		};
+			if (options.dominantColor) {
+				result.imgDominantColor = options.dominantColor;
+			}
 
-		if (options.page) {
-			result.start = options.page;
+			if (options.colorType) {
+				result.imgColorType = options.colorType;
+			}
+
+			if (options.safe) {
+				result.safe = options.safe;
+			}
+
+			return qs.stringify(result);
 		}
+	}]);
 
-		if (options.size) {
-			result.imgSize = options.size;
-		}
-
-		if (options.type) {
-			result.imgType = options.type;
-		}
-
-		if (options.dominantColor) {
-			result.imgDominantColor = options.dominantColor;
-		}
-
-		if (options.colorType) {
-			result.imgColorType = options.colorType;
-		}
-
-		if (options.safe) {
-			result.safe = options.safe;
-		}
-
-		return qs.stringify(result);
-	}
-}
+	return Client;
+}();
 
 module.exports = Client;
